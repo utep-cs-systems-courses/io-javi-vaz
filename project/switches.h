4cd7288db4718cf3c7c76a638427bd651e2ae353
javi_vaz <jvazquez9@miners.utep.edu>
2023-11-11 12:23:28 -0700

    Mini Piano Toy

--- project/switches.h
+++ project/switches.h
@@ -1,12 +1,15 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3/* switch1 is p1.3 */
-#define SWITCHES SW1/* only 1 switch on this board */
+#define SW1 1
+#define SW2 2
+#define SW3 4
+#define SW4 8
+
+#define SWITCHES 15
 
 void switch_init();
 void switch_interrupt_handler();
-
-extern char switch_state_down, switch_state_changed; /* effectively boolean */
+static char switch_update_interrupt_sense();
 
 #endif // included
